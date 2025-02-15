8a203aa8c09824425b6e3a77d0ae6ff571597b2a
JamzOhJamz <abstractdeveloperx@gmail.com>
2025-02-15 13:01:02 -0700

    Add highlight texture for Infinifurnace

--- Content/Tiles/Infinifurnace.cs
+++ Content/Tiles/Infinifurnace.cs
@@ -2,6 +2,7 @@
 using Terraria;
 using Terraria.DataStructures;
 using Terraria.Enums;
+using Terraria.GameContent.ObjectInteractions;
 using Terraria.ID;
 using Terraria.Localization;
 using Terraria.ModLoader;
@@ -11,10 +12,13 @@ namespace ProceduralOres.Content.Tiles;
 
 public class Infinifurnace : ModTile
 {
+    public override string HighlightTexture => "ProceduralOres/Content/Tiles/Infinifurnace_Highlight";
+    
     public override void SetStaticDefaults()
     {
         Main.tileFrameImportant[Type] = true;
         Main.tileLavaDeath[Type] = false;
+        TileID.Sets.HasOutlines[Type] = true;
         TileID.Sets.DisableSmartCursor[Type] = true;
 
         TileObjectData.newTile.CopyFrom(TileObjectData.Style3x2);
@@ -23,6 +27,11 @@ public class Infinifurnace : ModTile
         TileObjectData.addTile(Type);
 
         AddMapEntry(new Color(255, 100, 0), Language.GetText("ItemName.Furnace"));
+    }
+    
+    public override bool HasSmartInteract(int i, int j, SmartInteractScanSettings settings)
+    {
+        return true;
     }
 
     public override void NumDust(int x, int y, bool fail, ref int num)
